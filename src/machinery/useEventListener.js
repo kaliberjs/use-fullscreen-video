cadf82efa67f857ed00bf88abd530feb472a69c8
Albert Smit <31598745+AlbertSmit@users.noreply.github.com>
2024-05-23 15:02:18 +0200

    Update useEventListener.js

--- src/machinery/useEventListener.js
+++ src/machinery/useEventListener.js
@@ -2,11 +2,11 @@ export function useEventListener(name, callback) {
   React.useEffect(
     () => {
         if (name && callback) {
-            document.addEventListener(name, callback, false)
+          document.addEventListener(name, callback, false)
         }
 
         return () => {
-        document.removeEventListener(name, callback, false)
+          document.removeEventListener(name, callback, false)
         }
     }, 
     [name, callback]
